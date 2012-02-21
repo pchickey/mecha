8070e9fe1b6cdd9880c047938f851eb00d01f9a1
Pat Hickey <pat@galois.com>
2012-02-21 10:26:12 -0800

    Mecha.Examples.CSG: Add a simple polyhedron solid
    * based on OpenSCAD example polyhedron here:
    http://en.wikibooks.org/wiki/OpenSCAD_User_Manual/The_OpenSCAD_Language#polyhedron

--- Language/Mecha/Examples/CSG.hs
+++ Language/Mecha/Examples/CSG.hs
@@ -14,6 +14,8 @@ csg = unions
   , move ( 0, 8, 0) $ color (0.0, 0.8, 0.0, 1) $ cyl
   , move (-4, 8, 0) $ color (0.0, 0.8, 0.0, 1) $ rotateX (pi/2) cyl
   , move ( 4, 8, 0) $ color (0.0, 0.8, 0.0, 1) $ rotateY (pi/2) cyl
+  , move ( 0, 12, 0) $ color (0.0, 0.8, 0.0, 1) $ triprism
+  , move ( 0, 16, 0) $ color (0.0, 0.8, 0.0, 1) $ cutcube
   ]
 
 sphere' = sphere 2
@@ -22,3 +24,15 @@ sphereCube = intersection sphere' cube'
 cyl = moveZ (-1) $ cylinder 1 2
 cyl3 = unions [cyl, rotateX (pi / 2) cyl, rotateY (pi / 2) cyl]
 
+triprism = scaleAll (0.025) $ polyhedron pts tris
+  where
+  pts = [ (  0, -10, 60 )
+        , (  0,  10, 60 )
+        , (  0,  10,  0 )
+        , (  0, -10,  0 )
+        , ( 60, -10, 60 )
+        , ( 60,  10, 60 ) ]
+  tris = [ ( 0, 3, 2 ) , ( 0, 2, 1 ) , ( 3, 0, 4 ) , ( 1, 2, 5 )
+         , ( 0, 5, 4 ) , ( 0, 1, 5 ) , ( 5, 2, 4 ) , ( 4, 2, 3 ) ]
+
+cutcube = difference cube' $ move (0, 0, -0.5) triprism
